ef8a713563312464b74cdc694e8cd73c02c401c4
WeulerSilva <weulergamer@outlook.com>
2024-08-30 14:03:14 -0300

    finalizing market page

--- src/app/[locale]/Market/page.tsx
+++ src/app/[locale]/Market/page.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import { BlueDiv } from "../components/BlueDiv";
+import { TitleParagraph } from "../components/TitleParagraph";
 
 export default function Pages() {
     const t = useTranslations('market');
@@ -24,6 +25,24 @@ export default function Pages() {
                 </div>
             </div>
 
+            <div className="w-full mt-10 flex justify-center flex-col items-center">
+                <BlueDiv completBlue firstText={t("h1")} aboutStyle='hidden' />
+
+                <div className='w-full mt-7 px-6 my-4 flex justify-between items-start flex-col text-[15px] font-nexa 
+                                text-justify md:pl-6 md:space-x-10 md:w-[85%] md:flex-row xl:w-[65%]'>
+                    <div className="w-full md:w-[50%]">
+                        <TitleParagraph h1={t('m-h-1')} p={t('m-p-1')} />
+                        <TitleParagraph h1={t('m-h-2')} p={t('m-p-2')} />
+                        <TitleParagraph h1={t('m-h-3')} p={t('m-p-3')} />
+                    </div>
+
+                    <div className="w-full md:w-[50%]">
+                        <TitleParagraph h1={t('m-h-4')} p={t('m-p-4')} />
+                        <TitleParagraph h1={t('m-h-5')} p={t('m-p-5')} />
+                        <TitleParagraph h1={t('m-h-6')} p={t('m-p-6')} />
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
